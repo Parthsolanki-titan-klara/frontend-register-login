504afd1f561ba7f3e4799b71e012632e4440a77a
parth <solankiparth.sp1988@gmail.com>
2024-09-04 22:35:50 +0530

    refresh token api call working

--- src/interceptor/axiosInstance.jsx
+++ src/interceptor/axiosInstance.jsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { jwtDecode } from 'jwt-decode';
 import store from '../componant/store/store'; // Import the store directly
 import { setTokens } from '../componant/slices/authSlice';
 
@@ -8,44 +7,6 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
 });
-let refreshTokenTimeout;
-
-const scheduleRefreshToken = (expiresIn) => {
-    const state = store.getState();
-    const refreshToken = state.auth.refreshToken;
-
-    if (refreshTokenTimeout) {
-        clearTimeout(refreshTokenTimeout);
-    }
-    refreshTokenTimeout = setTimeout(async () => {
-        try {
-            const refreshTokenUrl = `${import.meta.env.VITE_API_BASE_URL}/refresh-token`;
-            const refreshTokenResponse = await axios.post(refreshTokenUrl, {}, {
-                headers: {
-                    'Authorization': `Bearer ${refreshToken}`,
-                },
-                withCredentials: true,
-            });
-
-            console.log('Refresh token response in schedule RefreshToken:', refreshTokenResponse);
-
-            if (refreshTokenResponse.status === 200) {
-                const newAccessToken = refreshTokenResponse.data.accessToken;
-                const newRefreshToken = refreshTokenResponse.data.refreshToken;
-                const expiresIn = refreshTokenResponse.data.expiresIn;
-
-                // Update tokens
-                store.dispatch(setTokens({ accessToken: newAccessToken, refreshToken: newRefreshToken }));
-                axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-
-                // Schedule the next refresh
-                scheduleRefreshToken(expiresIn);
-            }
-        } catch (error) {
-            console.error('Failed to refresh token:', error);
-        }
-    }, (expiresIn - 60) * 1000); // Refresh 1 minute before expiration
-};
 
 axiosInstance.interceptors.response.use(
     response => response,
@@ -60,6 +21,8 @@ axiosInstance.interceptors.response.use(
             try {
                 const state = store.getState();
                 const refreshToken = state.auth.refreshToken;
+                console.log('Refresh token from the store: ', refreshToken);
+                
                 const refreshTokenUrl = `${import.meta.env.VITE_API_BASE_URL}/refresh-token`;
                 const refreshTokenResponse = await axios.post(refreshTokenUrl, {}, {
                     headers: {
@@ -73,16 +36,14 @@ axiosInstance.interceptors.response.use(
                 if (refreshTokenResponse.status === 200) {
                     const newAccessToken = refreshTokenResponse.data.accessToken;
                     const newRefreshToken = refreshTokenResponse.data.refreshToken;
-                    const expiresIn = refreshTokenResponse.data.expiresIn;
 
                     // Update tokens
                     store.dispatch(setTokens({ accessToken: newAccessToken, refreshToken: newRefreshToken }));
                     axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
                     originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
 
-                    // Schedule the next refresh
-                    scheduleRefreshToken(expiresIn);
-
+                    console.log('Retry original request after refreshing token:', originalRequest);
+                    
                     return axiosInstance(originalRequest);
                 }
             } catch (err) {
@@ -92,13 +53,5 @@ axiosInstance.interceptors.response.use(
         return Promise.reject(error);
     }
 );
-
-// Initial token scheduling
-const state = store.getState();
-const refreshToken = state.auth.refreshToken;
-if (refreshToken) {
-    const initialExpiresIn = jwtDecode(refreshToken).exp;
-    scheduleRefreshToken(initialExpiresIn);
-}
 
 export default axiosInstance;
