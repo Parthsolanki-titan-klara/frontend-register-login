9ab8c4e70aaeacebe66b5736a6b95d525cabe73d
parth <solankiparth.sp1988@gmail.com>
2024-09-09 22:48:34 +0530

    now all functinality working fine

--- src/interceptor/axiosInstance.jsx
+++ src/interceptor/axiosInstance.jsx
@@ -17,6 +17,8 @@ axiosInstance.interceptors.response.use(
         console.log('Error in axiosInstance:', error);
         const specialUrl = '/allusers';
         const originalRequest = error.config;
+        console.log("Error code : ", error.response.status);
+
         if (error.response.status === 401 && !originalRequest._retry && originalRequest.url.includes(specialUrl)) {
             console.log('Refreshing token... while 401 error');
 
@@ -36,27 +38,25 @@ axiosInstance.interceptors.response.use(
 
                 console.log('Refresh token response:', refreshTokenResponse);
 
-                if (refreshTokenResponse.status === 200) {
-                    const { accessToken: newAccessToken, refreshToken: newRefreshToken } = response.data;
+                const newAccessToken = refreshTokenResponse.data.accessToken;
+                const newRefreshToken = refreshTokenResponse.data.refreshToken;
+                // Update tokens
+                store.dispatch(setTokens({ accessToken: newAccessToken, refreshToken: newRefreshToken }));
+                axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
+                originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
 
-                    // Update tokens
-                    store.dispatch(setTokens({ accessToken: newAccessToken, refreshToken: newRefreshToken }));
-                    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`;
-                    originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                console.log('Retry original request after refreshing token:', originalRequest);
 
-                    console.log('Retry original request after refreshing token:', originalRequest);
-
-                    return axiosInstance(originalRequest);
-                }
+                return axiosInstance(originalRequest);
             } catch (err) {
                 // console.log('Error in refreshing token:', err);
                 // Clear the Redux store and redirect to the login page
                 console.log('Error in refreshing token... clearing tokens');
-                
+
                 store.dispatch(clearTokens());
                 console.log("accesstoken : ", useSelector((state) => state.auth.accessToken));
                 console.log("refreshtoken : ", useSelector((state) => state.auth.refreshToken));
-                
+
                 useNavigate('/');
                 toast.error('Session expired. Please login again.');
 
